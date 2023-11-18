e889f3e1c5a4b9d7db39cc5804bca5d4571ee71d
tahirxx <59288568+tahirxx@users.noreply.github.com>
2023-11-18 15:24:49 -0500

    updated description text

--- src/data.jsx
+++ src/data.jsx
@@ -2,7 +2,7 @@ export default [
     {
         id: 1,
         title: "E-commerce Platform",
-        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        description: "Developed a comprehensive e-commerce platform using the MERN (MongoDB, Express.js, React, Node.js) stack. Implemented user authentication, product management, and order processing functionalities. Integrated secure payment gateways for seamless transactions. The responsive and intuitive user interface enhances the overall shopping experience, while the backend ensures efficient data storage and retrieval.",
         coverImg: "web1.webp",
         stack: "MERN Stack",
         date: "Aug 2023 - Sept 2023",
@@ -11,7 +11,7 @@ export default [
     {
         id: 2,
         title: "Task Management Dashboard",
-        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        description: "Designed and implemented a task management dashboard using the MEAN (MongoDB, Express.js, Angular, Node.js) stack. Users can create, edit, and delete tasks, with real-time updates reflecting changes across the platform. The Angular-based frontend offers a seamless and interactive user experience, while the backend handles data storage and business logic. The project demonstrates the developer's proficiency in both frontend and backend technologies.",
         coverImg: "web2.webp",
         stack: "MongoDB, Express.js, React, Node.js",
         date: "Aug 2023 - Sept 2023",
@@ -20,7 +20,7 @@ export default [
     {
         id: 3,
         title: "Blog Platform",
-        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        description: "Developed a feature-rich blog platform with a custom Content Management System (CMS) using React, Node.js, and GraphQL. Users can create, edit, and organize blog posts with a WYSIWYG editor. The GraphQL API efficiently fetches data, optimizing performance. The project highlights the developer's skills in creating complex applications with a focus on user-friendly content creation and management.",
         coverImg: "web3.webp",
         stack: "React and Node.js",
         date: "Aug 2023 - Sept 2023",
